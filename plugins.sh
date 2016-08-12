fb616aeeafda91046612a61057aba924da5be500
Gerónimo <geronimo.afonso@mikroways.net>
2016-08-12 11:32:25 -0300

    vars error

--- plugins.sh
+++ plugins.sh
@@ -2,8 +2,7 @@
 
 set -e
 IFS=$'\n'
-plug="https://github.com/Undev/redmine_issue_checklist.git plugins/redmine_issue_checklist"
-for plugin in $plug; do
+for plugin in $PLUGINS; do
   URL=`echo $plugin | awk -F " " '{print $1}'`
   FOLDER=`echo $plugin | awk -F " " '{print $2}'`
   if ! [ -d $FOLDER ]; then
