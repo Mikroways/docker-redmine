2c0e2e21b3c6a1d0ea29f9abf50ddd33257dbdc7
Gerónimo <geronimo.afonso@mikroways.net>
2016-08-16 11:10:00 -0300

    test

--- start.sh
+++ start.sh
@@ -3,9 +3,4 @@
 set -e
 /bin/bash /plugins.sh
 /bin/bash -c "envsubst < /tmp/configuration.yml > /usr/src/redmine/config/configuration.yml"
-
 /bin/bash /docker-entrypoint.sh $@
-
-#/bin/bash -c "envsubst '$SECRET_TOKEN $DELIVERY_METHOD $ADDRESS $PORT $AUTHENTICATION $DOMAIN $USER_NAME $PASSWORD $STARTTLS_AUTO' < /tmp/configuration.yml > /tmp/configuration2.yml"
-
-
