1093bf3142b82b65f0863ce40a54f574b06493ee
Gerónimo <geronimo.afonso@mikroways.net>
2016-08-18 11:24:26 -0300

    Error al copiar configuration.yml ya que la carpeta config no existe. "parche" creado

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 
 set -e
 /bin/bash /plugins.sh
+if ! [ -d config ]; then
+    mkdir config
+fi
 /bin/bash -c "envsubst < /tmp/configuration.yml > /usr/src/redmine/config/configuration.yml"
 /bin/bash /docker-entrypoint.sh $@
